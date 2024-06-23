91e4aba1a95a5714f6a547f0791dbf47c65e42d4
DanSilSant <aluno23298@ipt.pt>
2024-06-23 22:56:42 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFS_23298_23306.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
