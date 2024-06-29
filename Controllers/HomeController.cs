777fc5a5ec0c01f22077f6639a53b9006d55e8a4
hugo gonçalves <101368171+hgoncal2@users.noreply.github.com>
2024-06-29 02:10:54 +0100

    adicionado viewmodel Temas/Foto que irá ser usado para gerar uma lista de Objetos(Tema,Foto) que irá ser usada no index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,49 @@
-﻿using EFS_23298_23306.Models;
+﻿using EFS_23298_23306.Data;
+using EFS_23298_23306.Models;
+using EFS_23298_23306.ViewModel;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace EFS_23298_23306.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController>? _logger;
+        private readonly ApplicationDbContext _context;
+        
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public HomeController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
-            _logger = logger;
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+
+            var applicationDbContext = _context.Temas.Include(m => m.ListaFotos.Where(f => f.deleted != true)).Where(m => m.Deleted != true).OrderByDescending(m => m.DataCriacao);
+            ICollection<TemasFotoViewModel> TmVm = new List<TemasFotoViewModel>();
+            var lista = await applicationDbContext.ToListAsync();
+            if (lista != null)
+            {
+                if (lista.Any())
+                {
+                    foreach (var tema in lista)
+                    {
+                        if (tema.ListaFotos.Any()) {
+                            foreach(var foto in tema.ListaFotos)
+                        {
+                                TmVm.Add(new TemasFotoViewModel(tema.Nome, foto.Nome));
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            return View(TmVm);
+            
         }
 
         public IActionResult Privacy()
@@ -25,7 +53,7 @@ namespace EFS_23298_23306.Controllers
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
-        {
+        {  
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
---
+++ ViewModel/TemaFotoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using EFS_23298_23306.Models;
+
+namespace EFS_23298_23306.ViewModel
+{
+    public class TemasFotoViewModel
+    {
+        public int Id { get; set; }
+
+        public String Tema { get; set; }
+        public String Foto { get; set; }
+
+        public TemasFotoViewModel(String Tema,String Foto)
+        {
+
+            this.Tema = Tema;
+            this.Foto = Foto;
+        }
+    }
+}
