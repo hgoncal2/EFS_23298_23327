509a46e6bce34bb9907e0a8ed3051fef823d1114
DanSilSant <aluno23298@ipt.pt>
2024-07-14 16:29:00 +0100

    Desenvolvimento da página AboutUs Algumas correções no perfil (_PartialViewReservas)

---
+++ Controllers/AboutUsController.cs
@@ -0,0 +1,49 @@
+﻿using EFS_23298_23327.Data;
+using EFS_23298_23327.Models;
+using EFS_23298_23327.ViewModel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Xml.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EFS_23298_23327.Controllers
+{
+    [CustomAuthorize(Roles = "Admin,Anfitriao,Cliente")]
+    public class AboutUsController : Controller
+    {
+        private readonly UserManager<Utilizadores> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _context;
+        private Utilizadores userLogado { get; set; }
+
+
+        public AboutUsController(
+            UserManager<Utilizadores> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext context)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            return View();
+
+        }
+
+
+    }
+}
